c2ef45faa73acf4740f0ec5bda030470b89834a9
sruthi05 <psruthi0608@gmail.com>
2017-09-12 12:32:01 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main 
+{
+
+	public static void main(String[] args) 
+{
+		
+	/*Scanner s=new Scanner (System.in):
+	int n=s.nextInt();
+        int id[]=new id[n];
+	String fullName[]=new String[n];
+	for(int i=0;i<n;i++)
+	{
+		id[i]=s.nextInt();
+		fullName[i]=s.nextLine();
+		
+        */    
+
+}
+
+}
